49a7f0e57edf7cc0fd7ba3879b4393615ae4e53d
simov <simeonvelichkov@gmail.com>
2017-08-12 21:53:29 +0300

    Add href to the event link

--- mithril/components/card-slider.js
+++ mithril/components/card-slider.js
@@ -3,7 +3,10 @@ v.component['card-slider'] = {
   view: (vnode) =>
     m('.v-slider',
       vnode.attrs.events.map((event) =>
-        m('a.mdc-card mdc-card--theme-dark v-card',
+        m('a.mdc-card mdc-card--theme-dark v-card', {
+          href: '/event/' + event.id,
+          oncreate: m.route.link
+          },
           m('section.mdc-card__media', {
             style: 'background-image: url(' + event.photo + ')'
             },
